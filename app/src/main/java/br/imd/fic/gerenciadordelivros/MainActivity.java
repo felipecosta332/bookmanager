3a008da47b1c011b75c2f234e27b231b23df3377
Felipe Costa <felipecosta776@outlook.com>
2020-08-06 15:41:40 -0300

    first commit

---
+++ app/src/main/java/br/imd/fic/gerenciadordelivros/MainActivity.java
@@ -0,0 +1,14 @@
+package br.imd.fic.gerenciadordelivros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
