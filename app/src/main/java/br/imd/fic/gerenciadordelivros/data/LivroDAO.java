26ae5977923e99358a1fd89450e94c44d2dc2b03
Felipe Costa <felipecosta776@outlook.com>
2020-08-11 15:15:23 -0300

    third and final delivery

--- app/src/main/java/br/imd/fic/gerenciadordelivros/data/LivroDAO.java
+++ app/src/main/java/br/imd/fic/gerenciadordelivros/data/LivroDAO.java
@@ -94,7 +94,7 @@ public class LivroDAO {
 
         bd.update(LivroContract.TABLE_NAME,
                 values,
-                LivroContract.Columns._ID + "?",
+                LivroContract.Columns._ID + "=?",
                 new String[]{String.valueOf(livro.getId())}
                 );
     }
@@ -102,7 +102,7 @@ public class LivroDAO {
     public void delete(Livro livro) {
 
         bd.delete(LivroContract.TABLE_NAME,
-                LivroContract.Columns._ID + "?",
+                LivroContract.Columns._ID + "=?",
                 new String[]{String.valueOf(livro.getId())}
         );
 
